598ad704084b7ff1b5acff55ec75f605b17fe108
Ben Hardill <hardillb@gmail.com>
2023-08-31 20:49:46 +0100

    Add listener

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const { SerialPort } = require('serialport')
 const port = new SerialPort({
     path: '/dev/ttyUSB0',
     baudRate: 57600,
-  })
+  }, function (err) {
+    if (err) {
+      console.log(err)
+    }
+})
+
+port.on('data', (data) => {
+  console.log(data)
+})
